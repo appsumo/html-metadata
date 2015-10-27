1225ecf12fdf21bc58628c906d1b07b79a52ca59
Damian Hodgkiss <damian@hodgkiss.id.au>
2015-10-28 10:02:15 +1030

    join text with spaces to avoid joined-words

--- test-lang.js
+++ test-lang.js
@@ -2,6 +2,7 @@ var scrape = require('./index');
 
 scrape({
   uri: 'http://www.dicasodonto.com.br/2013/02/22/cuidados-com-anestesia-odontologica/',
+//  uri: 'http://www.chadboyda.com/',
   headers: {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'
   }
